a39ecbe9a9ff4ddfbd3b08fa9781a86c56391d9a
Thomas Ched <tom.chedru@gmail.com>
2024-10-20 09:20:37 +0000

    Commit 2 on test branch

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from unittest import main
 
 print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
 print("This is a commit test")
-
+print("This is a second test, adding a test branch to the repo")
 # Run unit tests automatically
 main(module='test_module', exit=False)
