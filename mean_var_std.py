8ec031a03fb77dadc57544072c67a6f3ba922497
Thomas Ched <tom.chedru@gmail.com>
2024-10-20 10:35:26 +0000

    need to pass as lists, put into list of dicts and prep answer

--- mean_var_std.py
+++ mean_var_std.py
@@ -13,15 +13,32 @@ def calculate(lst):
         mean_response["axis2"] = my_arr.mean(axis=1)
         mean_response["flattened"] = my_arr.mean()
         # variance
+        var_response = {}
+        var_response["axis1"] = my_arr.var(axis=0)
+        var_response["axis2"] = my_arr.var(axis=1)
+        var_response["flattened"] = my_arr.var()
 
         # standard dev
-
+        std_response = {}
+        std_response["axis1"] = my_arr.std(axis=0)
+        std_response["axis2"] = my_arr.std(axis=1)
+        std_response["flattened"] = my_arr.std()
         # max
-
+        max_response = {}
+        max_response["axis1"] = my_arr.max(axis=0)
+        max_response["axis2"] = my_arr.max(axis=1)
+        max_response["flattened"] = my_arr.max()
         # min
-
+        min_response = {}
+        min_response["axis1"] = my_arr.min(axis=0)
+        min_response["axis2"] = my_arr.min(axis=1)
+        min_response["flattened"] = my_arr.min()
         # sum
+        sum_response = {}
+        sum_response["axis1"] = my_arr.sum(axis=0)
+        sum_response["axis2"] = my_arr.sum(axis=1)
+        sum_response["flattened"] = my_arr.sum()
+        print(sum_response)
         
-
     return print("Done running")
 #    return calculations
