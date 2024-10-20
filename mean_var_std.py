9752affe2258b21303b7cb11be08e67f810eb874
Thomas Ched <tom.chedru@gmail.com>
2024-10-20 10:21:45 +0000

    mean done

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,27 @@
 import numpy as np
 
-def calculate(list):
+def calculate(lst):
+    if len(lst) != 9:
+        raise ValueError("List must contain nine numbers.")
+    else:
+        answer_list = []
+        my_arr = np.array(lst)
+        my_arr = np.reshape(my_arr, (3, 3))
+        #mean
+        mean_response = {}
+        mean_response["axis1"] = my_arr.mean(axis=0)
+        mean_response["axis2"] = my_arr.mean(axis=1)
+        mean_response["flattened"] = my_arr.mean()
+        # variance
 
+        # standard dev
 
+        # max
 
+        # min
 
-    return calculations
+        # sum
+        
+
+    return print("Done running")
+#    return calculations
